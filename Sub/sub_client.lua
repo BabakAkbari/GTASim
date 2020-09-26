1b5e32fdfed0fd72733fb1794f4ed81c9390adb4
Babak <babakakb@gmail.com>
2020-09-26 19:49:06 +0330

    plane_client: register sub command

---
+++ Sub/sub_client.lua
@@ -0,0 +1,49 @@
+-- register sub command
+-- a list of available vehicle models is documented in https://wiki.gtanet.work/index.php?title=Vehicle_Models 
+-- but it may not be complete
+RegisterCommand('sub', function(source, args)
+    -- account for the argument not being passed
+    local vehicleName = args[1] or 'adder'
+
+    -- check if the vehicle actually exists
+    if not IsModelInCdimage(vehicleName) or not IsModelAVehicle(vehicleName) then
+        TriggerEvent('chat:addMessage', {
+            args = { 'It might have been a good thing that you tried to spawn a ' .. vehicleName .. '. Who even wants their spawning to actually ^*succeed?' }
+        })
+
+        return
+    end
+
+    -- load the model
+    RequestModel(vehicleName)
+
+    -- wait for the model to load
+    while not HasModelLoaded(vehicleName) do
+        Wait(500) -- often you'll also see Citizen.Wait
+    end
+
+    -- get the player's position
+    local playerPed = PlayerPedId() -- get the local player ped
+    local pos = GetEntityCoords(playerPed) -- get the position of the local player ped
+
+    -- create the vehicle
+    local vehicle = CreateVehicle(vehicleName, pos.x, pos.y, pos.z, GetEntityHeading(playerPed), true, false)
+
+    -- set the player ped into the vehicle's driver seat
+    SetPedIntoVehicle(playerPed, vehicle, -1)
+
+    -- give the vehicle back to the game (this'll make the game decide when to despawn the vehicle)
+    SetEntityAsNoLongerNeeded(vehicle)
+
+    -- release the model
+    SetModelAsNoLongerNeeded(vehicleName)
+
+    -- set the vehicle's plate text
+    SetVehicleNumberPlateText(vehicle, "Rover")
+    
+    -- tell the player
+    TriggerEvent('chat:addMessage', {
+		args = { 'Woohoo! Enjoy your new ^*' .. vehicleName .. '!' }
+	})
+end, false)
+
