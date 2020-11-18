cf213b99de7d15beb441f7d5847875f553c890fe
Babak <babakakb@gmail.com>
2020-11-18 09:11:33 +0330

    fxmanifest: server manifest updated

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,22 +1,23 @@
-fx_version 'bodacious'
+fx_version 'cerulean'
 game 'gta5'
 
 author 'An awesome dude'
 description 'An awesome, but short, description'
 version '1.0.0'
 
-resource_type 'gametype' { name = 'My awesome game type!' }
+-- resource_type 'gametype' { name = 'My awesome game type!' }
+
 
 
 client_scripts{
     'Copter/copter_client.lua',
-    'Plane/plane_client.lua',
-    'Sub/sub_client.lua',
-    'Rover/rover_client.lua'
+    -- 'Plane/plane_client.lua',
+    -- 'Sub/sub_client.lua',
+    'Rover/rover_client.lua',
 }
 server_scripts {
-    'Copter/copter_server.js',
-    'Plane/plane_server.js',
-    'Sub/sub_server.js',
+    'copter_server.js',
+    -- 'Plane/plane_server.js',
+    -- 'Sub/sub_server.js',
     'Rover/rover_server.js'
 }
